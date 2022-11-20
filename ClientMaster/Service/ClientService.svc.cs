8437880f5cca30985edab48afe925184a2c58123
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-11-20 14:01:10 +0530

    First Commit

---
+++ ClientMaster/Service/ClientService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ClientMaster.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ClientService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public string DoWork()
+        {
+            return "Welcome to First RESt WCF App";
+        }
+
+    }
+}
